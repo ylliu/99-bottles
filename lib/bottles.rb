bae5f82102d68dc51a19c510730765b76ec736d1
ylliu <liuming1908@gmail.com>
2022-07-27 14:40:50 +0800

    Listing 1.4: Shameless Green

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,18 +1,26 @@
 class Bottles
-  def song
-    verses(99, 0)
-  end
-
-  def verses(hi, lo)
-    hi.downto(lo).map { |n| verse(n) }.join("\n")
-  end
-
-  def verse(n)
-    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
-      " of beer on the wall," +
-      "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
-      "#{n > 0 ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around" : "Go to the store and buy some more"}, " +
-      "#{n - 1 < 0 ? 99 : n - 1 == 0 ? 'no more' : n - 1} bottle#{'s' if n - 1 != 1}" +
-      " of beer on the wall.\n"
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of beer on the wall," +
+        "no more bottles of beer.\n" +
+        "Go to the store and buy some more, " +
+        "99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall," +
+        "1 bottle of beer.\n" +
+        "Take it down and pass it around, " +
+        "no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall," +
+        "2 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall," +
+        "#{number} bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "#{number - 1} bottles of beer on the wall.\n"
+    end
   end
 end
