4d693561f9932198bb37df7e71da44cac49952c0
ylliu <liuming1908@gmail.com>
2022-08-01 14:05:29 +0800

    Listing 4.5: 1st 2nd Phrases Differ

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -55,12 +55,12 @@ class BottlesTest < Minitest::Test
     assert_equal expected, Bottles.new.verses(99, 98)
   end
 
-  def test_verse_6
-    expected = "1 six-pack of beer on the wall," +
-      "1 six-pack of beer.\n" +
-      "Take one down and pass it around, " +
-      "5 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(6)
-  end
+  # def test_verse_6
+  #   expected = "1 six-pack of beer on the wall," +
+  #     "1 six-pack of beer.\n" +
+  #     "Take one down and pass it around, " +
+  #     "5 bottles of beer on the wall.\n"
+  #   assert_equal expected, Bottles.new.verse(6)
+  # end
 
 end
