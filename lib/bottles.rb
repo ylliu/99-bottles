f040d18c3917f81b3f4375c07a99c0ff2e9c5d67
ylliu <liuming1908@gmail.com>
2022-08-01 15:01:59 +0800

    Listing 4.28: Quantity Method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,8 +9,8 @@ class Bottles
     when 1
       "#{number} #{container(number)} of beer on the wall," +
         "#{number} #{container(number)} of beer.\n" +
-        "Take it down and pass it around, " +
-        "no more bottles of beer on the wall.\n"
+        "Take #{pronoun(number)} down and pass it around, " +
+        "#{quantity(number - 1)} bottles of beer on the wall.\n"
       # when 6
       #   "1 six-pack of beer on the wall," +
       #     "1 six-pack of beer.\n" +
@@ -19,8 +19,8 @@ class Bottles
     else
       "#{number} #{container(number)} of beer on the wall," +
         "#{number} #{container(number)} of beer.\n" +
-        "Take one down and pass it around, " +
-        "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
+        "Take #{pronoun(number)} down and pass it around, " +
+        "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
@@ -35,6 +35,22 @@ class Bottles
       "bottle"
     else
       "bottles"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number
     end
   end
 end
