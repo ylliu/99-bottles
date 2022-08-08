f3a4c84cc721363d99eee333bf16acad2b9b2514
ylliu <liuming1908@gmail.com>
2022-08-08 17:31:53 +0800

    Listing 5.28: Obsession Cured

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,11 @@
 class Bottles
   def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} of beer on the wall," +
-      "#{quantity(number)} #{container(number)} of beer.\n" +
-      "#{action(number)}" +
-      "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall," +
+      "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
+      "#{bottle_number.action}" +
+      "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
   end
 
   def verses(starting, ending)
